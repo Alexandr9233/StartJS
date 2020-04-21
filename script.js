3501d3999da1e9d8788d83c0b68d10744f38b623
Alexandr9233 <shurk923923@gmail.com>
2020-04-21 21:30:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4 / 0);
+// console.log('string' * 9);
+
+// let person = {
+//   name: 'John',
+//   age: 25,
+//   isMarried: false
+// };
+
+// console.log(person.age);
+// console.log(person["name"]);
+
+
+// let arr = ["plum.png", "orang.jpg", "apple.bmp"];
+
+// console.log(arr[1]);
+
+// //alert('Hello world');
+
+// //let answer = confirm('Are you ok?');
+
+// console.log(typeof (undefined));
+
+// let incr = 10,
+//   decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+// console.log(5 % 2);
+// console.log('2' == 2);
+// console.log('2' === 2);
+
+
+let isCheked = true,
+  isClouse = false;
+
+console.log(isCheked && isClouse);
+console.log(isCheked || isClouse);
